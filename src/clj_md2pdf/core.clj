51be7c691a6dfd86ff6388d36508bc4782b97f85
Michael Valdron <michael.valdron@gmail.com>
2020-08-06 06:15:30 -0400

    more source added.

--- src/clj_md2pdf/core.clj
+++ src/clj_md2pdf/core.clj
@@ -1,1 +1,18 @@
-(ns clj-md2pdf.core)
+(ns clj-md2pdf.core
+  (:require [clojure.java.io :as io]
+            [clj-htmltopdf.core :as htmltopdf]
+            [markdown-to-hiccup.core :refer [file->hiccup]]))
+
+;; TODO: Write other functions
+
+(defn ->pdf
+  "Wrapper for `->pdf` in `clj-htmltopdf` which creates parent directories if non-existant."
+  [in out & options]
+  (io/make-parents out)
+  (-> htmltopdf/->pdf
+      (partial in out)
+      (apply options)))
+
+(defn -main [& args]
+  ;; TODO: Write main function
+  (assert false "Not Implemented."))
--- test/clj_md2pdf/core_test.clj
+++ test/clj_md2pdf/core_test.clj
@@ -1,3 +1,19 @@
 (ns clj-md2pdf.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.test :refer :all]
+            [markdown-to-hiccup.core :refer [file->hiccup]]
             [clj-md2pdf.core :refer :all]))
+
+(deftest test-markdown
+  (testing
+   (do (println "\nTest #1:")
+       (-> (doto (file->hiccup "resources/md/doc.md")
+             (pprint))
+           (comp #(is %) not empty?)))))
+
+(deftest test-pdf
+  (testing
+   (do (println "\nTest #2:")
+       (-> (file->hiccup "resources/md/doc.md")
+           (->pdf "resources/pdf/doc.pdf" {:debug {:display-html? true}})
+           (comp #(is %) not nil?)))))

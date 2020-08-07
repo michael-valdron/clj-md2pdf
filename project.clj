479186c5a514f6e7dfad3d11c2a9b47e6e2befef
Michael Valdron <michael.valdron@gmail.com>
2020-08-07 02:36:51 -0400

    Deps changed.

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [hiccup "2.0.0-alpha2"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [markdown-to-hiccup "0.6.2"]
-                 [clj-htmltopdf "0.1-alpha7"]]
+                 [clj-htmltopdf "0.1-alpha7"]
+                 [eftest "0.5.9"]]
+  :plugins [[lein-eftest "0.5.9"]]
   :repl-options {:init-ns clj-md2pdf.core}
   :main clj-md2pdf.core)

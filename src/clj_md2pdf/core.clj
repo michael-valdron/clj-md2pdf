188317703913194dc69c20162719723f217f34df
Michael Valdron <michael.valdron@gmail.com>
2020-08-07 02:38:52 -0400

    new source added.

--- src/clj_md2pdf/core.clj
+++ src/clj_md2pdf/core.clj
@@ -1,18 +1,113 @@
 (ns clj-md2pdf.core
+  (:gen-class)
   (:require [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as string]
+            [clojure.set :refer [rename-keys]]
+            [clojure.spec.alpha :as spec]
             [clj-htmltopdf.core :as htmltopdf]
-            [markdown-to-hiccup.core :refer [file->hiccup]]))
+            [markdown-to-hiccup.core :refer [file->hiccup hiccup-in]]))
 
-;; TODO: Write other functions
+(declare read-md ->pdf)
+
+(def spec-pdf (spec/and string? #(re-matches #"^.*\.pdf$" %)))
+(def spec-md (spec/and string? #(re-matches #"^.*\.md$" %)))
+(def spec-edn (spec/and string? #(re-matches #"^.*\.edn$" %)))
+(def spec-edn-options (spec/map-of keyword? spec-edn))
+
+(def file-validator
+  [#(.. (io/as-file %) (exists)) "File must exists."])
+
+(def cli-options
+  [[nil "--doc DOC" "The edn file with base document attributes."
+    :validate file-validator]
+   [nil "--page PAGE" "The edn file with base page attributes."
+    :validate file-validator]
+   [nil "--styles STYLES" (str "The edn file with style attributes. "
+                               "(Optional) You can specify the following map within your edn file:\n"
+                               "{:styles [\"style.css\" ..]\n"
+                               " :fonts {:font-family \"<font-family>\"\n"
+                               "         :src \"/path/to/font.ttf\"}}")
+    :validate file-validator]
+   [nil "--watermark WATERMARK" "When specified creates a watermark with the attributes in the given edn."
+    :validate file-validator]
+   [nil "--objects OBJECTS" "The edn file with multiple object attributes to create graphics."
+    :validate file-validator]
+   ["-d" "--debug" "Enables debug mode."]
+   ["-l" "--logging" "Enables logging"]
+   ["-h" "--help" "Shows summary of the usage."]])
+
+(defn get-body-contents
+  [hiccup]
+  (->> (hiccup-in hiccup :body)
+       (drop 2)))
+
+(defn hiccup-out
+  [hiccup]
+  (->> hiccup
+       (into [:body {}])
+       (conj [:html {} [:head {}]])))
+
+(defn update-debug
+  [options]
+  (if (nil? (:debug options))
+    options
+    (update options :debug #(hash-map :display-html? %
+                                      :display-options? %))))
+
+(defn parse-edn-opts
+  [edn-options]
+  (assert (spec/valid? spec-edn-options edn-options)
+          "Invalid EDN files found.")
+  edn-options)
+
+(defn parse-options
+  [options]
+  (let [option-attrs (->> [:doc :page :styles :watermark :objects]
+                          (select-keys options)
+                          (parse-edn-opts)
+                          (reduce-kv #(assoc %1 %2 ((comp read-string slurp) %3)) {})
+                          (into (select-keys options [:debug :logging])))]
+    (-> option-attrs
+        (rename-keys {:logging :logging?})
+        (update-debug))))
+
+(defn render-pdf
+  [out ins options]
+  (let [hiccup (apply read-md ins)]
+    (->> options
+         (parse-options)
+         (->pdf hiccup out))))
+
+(defn read-md
+  "Reads the markdown file(s) into hiccup data structures."
+  [in & ins]
+  (assert (and (spec/valid? spec-md in)
+               (every? #(spec/valid? spec-md %) ins))
+          "Invalid markdown files found.")
+  (->> (into [in] ins)
+       (mapcat (comp get-body-contents file->hiccup))
+       (hiccup-out)))
 
 (defn ->pdf
-  "Wrapper for `->pdf` in `clj-htmltopdf` which creates parent directories if non-existant."
-  [in out & options]
+  "Wrapper for `->pdf` in `clj-htmltopdf` which creates parent directories 
+   if non-existant."
+  [hiccup out options]
+  (assert (spec/valid? spec-pdf out) "PDF filename invalid.")
   (io/make-parents out)
-  (-> htmltopdf/->pdf
-      (partial in out)
-      (apply options)))
+  (if (empty? options)
+    (htmltopdf/->pdf hiccup out)
+    (htmltopdf/->pdf hiccup out options)))
 
 (defn -main [& args]
-  ;; TODO: Write main function
-  (assert false "Not Implemented."))
+  (let [arg-opts (parse-opts args cli-options)]
+    (cond
+      (:errors arg-opts) (println (string/join "\n" (:errors arg-opts)))
+      (get-in arg-opts [:options :help]) (println (:summary arg-opts))
+      :else (if (< (count (:arguments arg-opts)) 2)
+              (println "Missing required arguments 'out' and/or 'in'.")
+              (let [[out & ins] (:arguments arg-opts)]
+                (println "Rendering PDF..")
+                (render-pdf out ins (:options arg-opts))
+                (println "done.")
+                (shutdown-agents))))))
--- test/clj_md2pdf/core_test.clj
+++ test/clj_md2pdf/core_test.clj
@@ -1,19 +1,56 @@
 (ns clj-md2pdf.core-test
-  (:require [clojure.pprint :refer [pprint]]
-            [clojure.test :refer :all]
+  (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as spec]
             [markdown-to-hiccup.core :refer [file->hiccup]]
             [clj-md2pdf.core :refer :all]))
 
+;; Library access tests
 (deftest test-markdown
   (testing
-   (do (println "\nTest #1:")
-       (-> (doto (file->hiccup "resources/md/doc.md")
-             (pprint))
-           (comp #(is %) not empty?)))))
+   (is (-> (file->hiccup "resources/md/doc.md")
+           (comp not empty?)))))
 
 (deftest test-pdf
   (testing
-   (do (println "\nTest #2:")
-       (-> (file->hiccup "resources/md/doc.md")
-           (->pdf "resources/pdf/doc.pdf" {:debug {:display-html? true}})
-           (comp #(is %) not nil?)))))
+   (is (-> (file->hiccup "resources/md/doc.md")
+           (->pdf "resources/pdf/doc.pdf" {})
+           (comp not nil?)))))
+
+;; Validation tests
+(deftest test-pdf-validation
+  (testing
+   (is (= true (spec/valid? spec-pdf "path/to/a/pdf/file.pdf")))))
+
+(deftest test-md-validation
+  (testing
+   (is (= true (spec/valid? spec-md "path/to/a/md/file.md")))))
+
+(deftest test-edn-validation
+  (testing
+   (is (= true (spec/valid? spec-edn "path/to/a/md/file.edn")))))
+
+(deftest test-edn-options-validation
+  (testing
+   (let [edn-map {:doc "path/to/doc.edn"
+                  :page "path/to/page.edn"
+                  :styles "path/to/styles.edn"}]
+     (is (= true (spec/valid? spec-edn-options edn-map))))))
+
+(deftest test-edn-options-invalidation
+  (testing
+   (let [edn-map {:doc :adoc
+                  :page "path/to/page.edn"
+                  :styles 2}]
+     (is (= false (spec/valid? spec-edn-options edn-map))))))
+
+;; Option Parsing
+(deftest test-parse-options
+  (testing
+   (let [edn-map {:doc "resources/edn/doc.edn"
+                  :debug true}
+         sol-map {:doc  (-> (:doc edn-map)
+                            (slurp)
+                            (read-string))
+                  :debug {:display-html? true
+                          :display-options? true}}]
+     (is (= sol-map (parse-options edn-map))))))

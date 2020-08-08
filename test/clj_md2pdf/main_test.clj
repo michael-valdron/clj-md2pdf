5be46c05a9b5c43a552df46dac249b99feb45913
Michael Valdron <michael.valdron@gmail.com>
2020-08-08 08:23:47 -0400

    more test edn files.

--- test/clj_md2pdf/main_test.clj
+++ test/clj_md2pdf/main_test.clj
@@ -5,6 +5,8 @@
 (def doc-out "resources/pdf/doc.pdf")
 (def one-md-in "resources/md/doc.md")
 (def doc-metadata "resources/edn/doc.edn")
+(def page-options "resources/edn/page.edn")
+(def styles-props "resources/edn/styles.edn")
 
 (deftest test-one-file-run
   (testing
@@ -12,5 +14,8 @@
        (is true))))
 
 (deftest test-one-file-with-attrs-run
-  (testing (do (-main doc-out one-md-in "--doc" doc-metadata)
+  (testing (do (-main doc-out one-md-in 
+                      "--doc" doc-metadata
+                      "--page" page-options
+                      "--styles" styles-props)
                (is true))))
